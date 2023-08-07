337dda61cadacd1fc5d7d72cefb725112fdc4f08
vathevitechguy <aromosevictor@gmail.com>
2023-08-07 01:53:02 -0400

    Sass config

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Navbar } from '../components/organisms';
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div>
+      <Navbar />
+      <h3>Page Not Found!</h3>
+    </div>
+  );
 };
 
 export default Error;
