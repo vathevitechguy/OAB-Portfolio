4692099f5c309c639246b533e34215c2ea53ee1a
vathevitechguy <aromosevictor@gmail.com>
2023-08-10 11:08:56 -0400

    . . .

--- src/components/organisms/MyWork/MyWork.jsx
+++ src/components/organisms/MyWork/MyWork.jsx
@@ -3,6 +3,7 @@ import './MyWork.scss';
 import Publications from '../../../assets/publications.png';
 import Blog from '../../../assets/blog.png';
 import Github from '../../../assets/github.png';
+import { Link } from 'react-router-dom';
 
 const MyWork = () => {
   return (
@@ -11,17 +12,28 @@ const MyWork = () => {
       <div className="MyWork_wrapper">
         <div className="work">
           <img src={Publications} alt="" />
-          <h5>Publications</h5>
+          <h5>
+            <Link to="https://scholar.google.com/citations?user=gnYqDV8AAAAJ&hl=en">
+              Publications
+            </Link>
+          </h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
         <div className="work">
           <img src={Blog} alt="" />
-          <h5>Personal Blog</h5>
+
+          <h5>
+            {' '}
+            <Link to="/personal-blog">Personal Blog</Link>
+          </h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
         <div className="work">
           <img src={Github} alt="" />
-          <h5>Projects</h5>
+
+          <h5>
+            <Link to="https://github.com/Dare-Badejo-001">Projects</Link>
+          </h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
       </div>
