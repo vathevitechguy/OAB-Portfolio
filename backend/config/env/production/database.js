f787e31c18c1adbaa0fb4df51d8d84dc49ee9822
vathevitechguy <aromosevictor@gmail.com>
2023-08-21 10:23:12 -0400

    . . .

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,17 +1,18 @@
-const { parse } = require("pg-connection-string");
+// const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  //   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
   return {
     connection: {
       client: "postgres",
       connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        user: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        ssl: env.bool("DATABASE_SSL", true),
       },
       debug: false,
     },
