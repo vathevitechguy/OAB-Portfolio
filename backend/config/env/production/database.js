74fed2d08750a8e981bdf1aa23e271108e971514
vathevitechguy <aromosevictor@gmail.com>
2023-08-21 10:48:46 -0400

    . . .

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -12,7 +12,7 @@ module.exports = ({ env }) => {
         database: env("DATABASE_NAME"),
         user: env("DATABASE_USERNAME"),
         password: env("DATABASE_PASSWORD"),
-        ssl: env.bool("DATABASE_SSL", true),
+        // ssl: env.bool("DATABASE_SSL", true),
       },
       debug: false,
     },
